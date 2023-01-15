c47224e43937bdaade1c5f0f1b23b48034d3ea8f
minkyung <minkyung7365@gmail.com>
2023-01-15 21:30:56 +0900

    initial commit

--- train_torch.py
+++ train_torch.py
@@ -10,8 +10,8 @@ from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.core.lightning import LightningModule
 from torch.utils.data import DataLoader, Dataset
 from transformers.optimization import AdamW, get_cosine_schedule_with_warmup
-from transformers import PreTrainedTokenizerFast, GPT2LMHeadModel
-
+# from transformers import PreTrainedTokenizerFast, GPT2LMHeadModel
+# abc
 parser = argparse.ArgumentParser(description='Simsimi based on KoGPT-2')
 
 parser.add_argument('--chat',
